3be2319f9cca67fa9da4902ab254c352e0cb1180
David <whitmadm@gmail.com>
2021-04-20 15:54:42 -0700

    1.2.1 Fixes a Vortex soft crash when a mod has a version that is missing "major", "minor", and/or "patch" in the new 0.95a version schema

--- index.js
+++ index.js
@@ -2,77 +2,17 @@
 const Promise = require('bluebird');
 const path = require('path');
 const rjson = require('relaxed-json');
-const { fs, log, util, selectors, actions } = require('vortex-api');
+const { fs, log, util, selectors } = require('vortex-api');
 const winapi = require('winapi-bindings');
 const semver = require('semver');
 
 const GAME_ID = 'starsector';
-const MOD_INFO_FILE = "mod_info.json";
-const VERSION_CHECKER_FILE_EXT = ".version";
-const ATTR_FORUM_THREAD_ID = "forumThreadId";
+const MOD_INFO_FILE = "mod_info.json"
+const VERSION_CHECKER_FILE_EXT = ".version"
 
 // Adapted from https://stackoverflow.com/a/24518413/1622788
 // Removes '#'s, which are used as comments by the game's json parser
 const COMMENT_STRIPPING_REGEX = /((["'])(?:\\[\s\S]|.)*?\2|\#(?![*\#])(?:\\.|\[(?:\\.|.)\]|.)*?\#)|\#.*?$|\#\*[\s\S]*?\*\#/gm;
-
-const TEST_MIGRATION = false
-/**
- * @param {import('vortex-api/lib/types/api').IExtensionContext} context
- */
-function main(context) {
-  context.registerGame({
-    id: GAME_ID,
-    name: 'Starsector',
-    mergeMods: true,
-    queryPath: findGame,
-    queryModPath: () => 'mods',
-    logo: 'gameart.jpg',
-    executable: () => 'starsector.exe',
-    requiredFiles: [
-      'starsector.exe',
-    ]
-  });
-
-  // Not working perfectly, seems to add errors to Vortex's state, which it complains endlessly about.
-  // context.registerMigration(old => migrateFrom_1_1_0(context.api, old));
-
-  context.registerInstaller(
-    'starsector',
-    50,
-    testSupportedContent,
-    (files, destination, gameId, progress) => installContent(context.api, files, destination, gameId, progress));
-
-  // Based on https://github.com/Nexus-Mods/Vortex/blob/fc439bda319430b0891151db2d6505ab79128872/src/extensions/nexus_integration/index.tsx#L804
-  context.registerAction('mods-action-icons', 900, 'about', {}, 'Open on Starsector Forums',
-    instanceIds => {
-      if (!isVortexInStarsectorMode(context)) {
-        return false;
-      }
-
-      if (TEST_MIGRATION) {
-        migrateFrom_1_1_0(context.api, '1.0.0'); // Just for testing
-      }
-
-      const forumThreadId = getModAttribute(context, instanceIds, ATTR_FORUM_THREAD_ID)
-
-      if (forumThreadId != undefined) {
-        log('info', 'Opening mod forum id ' + forumThreadId, {});
-
-        // Based on https://github.com/Nexus-Mods/Vortex/blob/063c53fc220beb95ccc1e49ef33174f1443f2e69/src/extensions/nexus_integration/eventHandlers.ts#L142
-        util.opn('https://fractalsoftworks.com/forum/index.php?topic=' + forumThreadId)
-          .catch(err => undefined);
-        return true;
-      } else {
-        return false;
-      }
-    },
-    instanceIds => {
-      return isVortexInStarsectorMode(context) &&
-        getModAttribute(context, instanceIds, 'forumThreadId') != null;
-    });
-
-  return true;
-}
 
 /**
  * @returns {string | Promise<String>}
@@ -131,9 +71,9 @@ async function installContent(
       // Function to get a value from mod_info.json by key
       const getAttr = key => {
         try {
-          return parsed[key].toString().trim();
+          return parsed[key];
         } catch (err) {
-          log('info', 'attribute missing in ' + MOD_INFO_FILE, { key, err });
+          log('info', 'attribute missing in ' + MOD_INFO_FILE, { key });
           return "";
         }
       }
@@ -142,13 +82,13 @@ async function installContent(
       const contentModId = getAttr('id');
       if (contentModId === undefined) {
         return Promise.reject(
-          new util.DataInvalid('invalid or unsupported ' + MOD_INFO_FILE));
+          new util.DataInvalid('Missing, invalid or unsupported ' + MOD_INFO_FILE));
       }
 
       outputPath = (modInfoFile.indexOf(MOD_INFO_FILE) > 0)
         ? path.basename(path.dirname(modInfoFile))
         : Promise.reject(
-          new util.DataInvalid('invalid or unsupported ' + MOD_INFO_FILE));
+          new util.DataInvalid('Missing, invalid or unsupported ' + MOD_INFO_FILE));
 
       // Don't overwrite name because authors tend to put "beta 1" or "RC1" or "WIP 3"
       // in the Nexus or archive name, but not in the metadata name or metadata version.
@@ -160,11 +100,39 @@ async function installContent(
       // });
 
       // Set the mod version based on mod_info.json
-      attrInstructions.push({
-        type: 'attribute',
-        key: 'version',
-        value: getAttr('version').trim(),
-      });
+      var version = getAttr('version')
+      try {
+        // Works if using old schema where version is just a string
+        version = version.trim()
+      } catch {
+        // Else use new schema where version is an object with major, minor, patch.
+        try {
+          var versionElements = [];
+          
+          if (version["major"] != null) {
+            versionElements.push(version["major"].toString());
+          }
+
+          if (version["minor"] != null) {
+            versionElements.push(version["minor"].toString());
+          }
+
+          if (version["patch"] != null) {
+            versionElements.push(version["patch"].toString());
+          }
+
+          version = versionElements.join('.');
+        } catch {
+        }
+      }
+
+      if (typeof (version) === 'string') {
+        attrInstructions.push({
+          type: 'attribute',
+          key: 'version',
+          value: version,
+        });
+      }
 
       // Description is fairly hidden in the UI, and we don't want to overwrite it
       // if it's being set from Nexus Mods.
@@ -183,19 +151,51 @@ async function installContent(
 
       return Promise.resolve(attrInstructions);
     })
-    .then(async attrInstructions => {
-      const filePaths = files.map(file => path.join(destinationPath, file));
-      let forumThreadId = await tryGetForumThreadIdFromModFiles(filePaths, destinationPath, contextApi);
+    .then(attrInstructions => {
 
-      if (forumThreadId != null) {
-        attrInstructions.push({
-          type: 'attribute',
-          key: ATTR_FORUM_THREAD_ID,
-          value: forumThreadId,
-        });
+      // Read the version file, if it exists
+      const versionCheckerFile = files.find(file => path.extname(file) === VERSION_CHECKER_FILE_EXT);
+      log('info', 'Found version checker file: ' + versionCheckerFile)
+
+      if (versionCheckerFile) {
+        const contentFile = path.join(destinationPath, versionCheckerFile);
+
+        return fs.readFileAsync(contentFile, { encoding: 'utf8' })
+          .then(versionCheckerData => {
+            let parsedVerCheckData;
+            try {
+              // Strip '#' comments using regex, then parse using relaxed-json
+              parsedVerCheckData = rjson.parse(versionCheckerData.replace(COMMENT_STRIPPING_REGEX, "$1"));
+            } catch (err) {
+              const errMsg = versionCheckerFile + ' invalid: ' + err.message
+              log('warn', errMsg);
+
+              contextApi.showErrorNotification(errMsg, {});
+              return Promise.resolve(attrInstructions);
+            }
+
+            // Function to get a value from mod_info.json by key
+            const getAttr = key => {
+              try {
+                return parsedVerCheckData[key];
+              } catch (err) {
+                log('info', 'attribute missing in ' + versionCheckerFile, { key });
+                return "";
+              }
+            }
+
+            // Set the forum thread id based on the Version Checker file
+            attrInstructions.push({
+              type: 'attribute',
+              key: 'forumThreadId',
+              value: getAttr('modThreadId'),
+            });
+
+            return Promise.resolve(attrInstructions);
+          })
+      } else {
+        return Promise.resolve(attrInstructions);
       }
-
-      return Promise.resolve(attrInstructions);
     })
     .then(attrInstructions => {
       let instructions = attrInstructions.concat(files.filter(file =>
@@ -209,40 +209,77 @@ async function installContent(
     });
 }
 
-
-async function tryGetForumThreadIdFromModFiles(filePaths, destinationPath, contextApi) {
-  // Read the version file, if it exists
-  const versionCheckerFile = filePaths.find(file => path.extname(file) === VERSION_CHECKER_FILE_EXT);
-  log('info', 'Found version checker file: ' + versionCheckerFile)
-
-  if (versionCheckerFile) {
-    let versionCheckerData = await fs.readFileAsync(versionCheckerFile, { encoding: 'utf8' })
-    let parsedVerCheckData;
-    try {
-      // Strip '#' comments using regex, then parse using relaxed-json
-      parsedVerCheckData = rjson.parse(versionCheckerData.replace(COMMENT_STRIPPING_REGEX, "$1"));
-    } catch (err) {
-      const errMsg = versionCheckerFile + ' invalid: ' + err.message
-      log('warn', errMsg);
-
-      contextApi.sendNotification({ type: 'warning', title: 'Unable to get forum thread id', message: errMsg });
-      return null;
-    }
-
-    // Function to get a value from mod_info.json by key
-    const getAttr = key => {
-      try {
-        return parsedVerCheckData[key].toString().trim();
-      } catch (err) {
-        log('info', 'attribute missing in ' + versionCheckerFile, { key, err });
-        return "";
-      }
-    }
-
-    return getAttr('modThreadId');
-  } else {
-    return null;
+// From https://github.com/Nexus-Mods/vortex-games/blob/296abf250fdc1c57314791e704d14a5165695dec/game-bladeandsorcery/index.js#L403
+function migrateFrom_1_1_0(api, oldVersion) {
+  if (semver.gt(oldVersion, '1.1.0')) {
+    return Promise.resolve();
   }
+
+  const state = api.store.getState();
+  const mods = util.getSafe(state, ['persistent', 'mods', GAME_ID], {});
+  const modKeys = Object.keys(mods);
+
+  if (modKeys.length === 0) {
+    return Promise.resolve();
+  }
+}
+
+/**
+ * @param {import('vortex-api/lib/types/api').IExtensionContext} context
+ */
+function main(context) {
+  context.registerGame({
+    id: GAME_ID,
+    name: 'Starsector',
+    mergeMods: true,
+    queryPath: findGame,
+    queryModPath: () => 'mods',
+    logo: 'gameart.jpg',
+    executable: () => 'starsector.exe',
+    requiredFiles: [
+      'starsector.exe',
+    ]
+  });
+
+  context.registerMigration(old => migrateFrom_1_1_0(context.api, old));
+
+  context.registerInstaller(
+    'starsector',
+    50,
+    testSupportedContent,
+    (files, destination, gameId, progress) => installContent(context.api, files, destination, gameId, progress));
+
+  // Based on https://github.com/Nexus-Mods/Vortex/blob/fc439bda319430b0891151db2d6505ab79128872/src/extensions/nexus_integration/index.tsx#L804
+  context.registerAction('mods-action-icons', 900, 'nexus', {}, 'Open on Starsector Forums',
+    instanceIds => {
+      const state = context.api.store.getState();
+
+      if (!isVortexInStarsectorMode(context)) {
+        return false;
+      }
+
+      const forumThreadId = getModAttribute(context, instanceIds, 'forumThreadId')
+      const error = getModAttribute(context, instanceIds, 'forumThreadIdError')
+
+      if (forumThreadId != undefined) {
+        log('info', 'Opening mod forum id ' + forumThreadId, {});
+
+        // Based on https://github.com/Nexus-Mods/Vortex/blob/063c53fc220beb95ccc1e49ef33174f1443f2e69/src/extensions/nexus_integration/eventHandlers.ts#L142
+        util.opn('https://fractalsoftworks.com/forum/index.php?topic=' + forumThreadId)
+          .catch(err => undefined);
+        return true;
+      } else if (error != null) {
+        context.api.showErrorNotification(error, {});
+      } else {
+        return false;
+      }
+    },
+    instanceIds => {
+      return isVortexInStarsectorMode(context) &&
+        getModAttribute(context, instanceIds, 'forumThreadId') != null;
+    });
+
+  return true;
 }
 
 function getModAttribute(context, instanceIds, attrKey) {
@@ -272,81 +309,6 @@ function getSafe(state, path, fallback) {
     }
   }
   return current;
-}
-
-// From https://github.com/Nexus-Mods/vortex-games/blob/05f49db49c57ff7d3fc643e72210fe31621391c6/game-dmc5/index.js#L258
-function walkAsync(dir) {
-  let entries = [];
-  return fs.readdirAsync(dir).then(files => {
-    return Promise.map(files, file => {
-      const fullPath = path.join(dir, file);
-      return fs.statAsync(fullPath).then(stats => {
-        if (stats.isDirectory()) {
-          return walkAsync(fullPath)
-            .then(nestedFiles => {
-              entries = entries.concat(nestedFiles);
-              return Promise.resolve();
-            })
-        } else {
-          entries.push(fullPath);
-          return Promise.resolve();
-        }
-      });
-    });
-  })
-    .then(() => Promise.resolve(entries));
-}
-
-// From https://github.com/Nexus-Mods/vortex-games/blob/296abf250fdc1c57314791e704d14a5165695dec/game-bladeandsorcery/index.js#L403
-function migrateFrom_1_1_0(api, oldVersion) {
-  if (semver.gt(oldVersion, '1.1.0')) {
-    return Promise.resolve();
-  }
-
-  log('info', 'Starting migration from extension v' + oldVersion, {});
-  const state = api.store.getState();
-  const mods = util.getSafe(state, ['persistent', 'mods', GAME_ID], {});
-  const modKeys = Object.keys(mods);
-
-  if (modKeys.length === 0) {
-    return Promise.resolve();
-  }
-
-  const decimalRegex = /^[-+]?[0-9]+\.[0-9]+$/;
-  const modsToMigrate = modKeys
-    .map(key => mods[key])
-    .filter(mod => {
-      if (TEST_MIGRATION) return true;
-
-      let threadId = mod.attributes[ATTR_FORUM_THREAD_ID];
-      return threadId == null || !threadId.toString().match(decimalRegex);
-    });
-
-  if (modsToMigrate.length === 0) {
-    return Promise.resolve();
-  }
-
-  const stagingFolder = selectors.installPathForGame(state, GAME_ID);
-
-  return Promise.each(modsToMigrate, mod => {
-    const modPath = path.join(stagingFolder, mod.installationPath);
-    return walkAsync(modPath)
-      .then(files => {
-        log('info', 'Searching mod files for version. Mod dir: ' + modPath, {});
-        // tryGetForumThreadIdFromModFiles logs and shows errors, no need to do so here
-        tryGetForumThreadIdFromModFiles(files, modPath, api)
-          .then(forumThreadId => {
-
-            if (forumThreadId != null) {
-              // From https://github.com/Nexus-Mods/vortex-games/blob/296abf250fdc1c57314791e704d14a5165695dec/game-bladeandsorcery/index.js#L444
-              api.store.dispatch(actions.setModAttribute(GAME_ID, mod.id, ATTR_FORUM_THREAD_ID, forumThreadId));
-              log('info', 'Migrated mod ' + mod.id + ', adding forumThreadId: ' + forumThreadId, {});
-            }
-
-            return Promise.resolve();
-          });
-      });
-  });
 }
 
 module.exports = {
